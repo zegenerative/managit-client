0047752c6320e7c2d4a2a98e7ea6fc6e21ec9084
zegenerative <zegerdevos@gmail.com>
2019-12-02 12:35:40 +0100

    Setup main page with login link

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Link to="https://github.com/login/oauth/authorize?client_id=YOUR_CLIENT_ID">
+          <button>
+            Login with github
+          </button>
+        </Link>
+      </div>
+    )
+  }
 }
-
-export default App;
