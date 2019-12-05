1a60c57bd1af6f077bae41c8741606834602083d
zegenerative <zegerdevos@gmail.com>
2019-12-05 12:32:39 +0100

    Change path again

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
             Login with github
           </a>
         </button>
-        <Route path={`/home`} component={ Home }/>
+        <Route exact path={`/home.html`} component={ Home }/>
       </div>
     )
   }
