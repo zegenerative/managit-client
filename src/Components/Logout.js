8e916b317289d22c2d7f5eb06cf1067cd009614b
zegenerative <zegerdevos@gmail.com>
2019-12-06 18:39:31 +0100

    Logout from github

---
+++ src/Components/Logout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Logout extends Component {
+
+    render() {
+        return (
+            <div>
+                <button>
+                    <a href={`https://github.com/logout`}>
+                        Logout
+                    </a>
+                </button>
+            </div>
+        )
+    }
+}
